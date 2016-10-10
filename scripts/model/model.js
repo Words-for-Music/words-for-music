1b41a9321f4b62e918e74240058dd06e9d547e1d
JPGothro <jpgothro@gmail.com>
2016-10-10 13:07:39 -0700

    server side error happening

--- scripts/model/model.js
+++ scripts/model/model.js
@@ -1,14 +1,15 @@
-var $ = require('jquery');
-var request = $.ajax({
-  // TODO: This ajax request currently duplicates some code contained in the proxyGenius function in server.js; it needs to be refactored a bit so the two requests properly work together.
-  url: 'https://api.genius.com/',
-  method: 'GET',
-  headers: {
-    Authorization: 'Bearer ' + ACCESS_TOKEN,
-    'Content-Type': 'application/json'
-  }
-}).done(function(returnedData) {
-  console.log(returnedData);
-}).fail(function(jqxhr, status) {
-  console.log('Call failed: ' + status, jqxhr);
-});
+// put this in an IIFE
+
+// Make a request to get song data
+var request = function() {
+  $.ajax({
+    url: '/genius/dummyData',
+    method: 'GET'
+  }).done(function(returnedData) {
+    console.log('model.js AJAX request: ', returnedData);
+  }).fail(function(jqxhr, status) {
+    console.log('model.js AJAX request Call failed: ' + status, jqxhr);
+  });
+};
+
+request();
--- server.js
+++ server.js
@@ -1,18 +1,29 @@
+
 var express = require('express'),
   requestProxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
   app = express();
 
 function proxyGenius(request, response) {
+  console.log('proxyGenius starting ', request.params);
   (requestProxy({
-    url: 'https://api.genius.com/' + request.params[0],
-    headers: {Authorization: 'Bearer ' + process.env.ACCESS_TOKEN}
+    url: 'https://api.genius.com',
+    type: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + process.env.ACCESS_TOKEN,
+      'Content-Type': 'application/json'}
   }))(request, response);
-}
+  // console.log('proxyGenius after proxy call: ', request.url);
+};
 
 app.get('/genius/*', proxyGenius);
 
 app.use(express.static('./'));
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: '.'});
+});
 
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!');
