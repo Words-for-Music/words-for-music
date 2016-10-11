6ccc523f7a6d8b017bf2d40afe34e91a4f265641
TomTimmel <tom.t.timmel@gmail.com>
2016-10-10 17:32:05 -0700

    added routing capability

---
+++ scripts/controller/controller.js
@@ -0,0 +1,27 @@
+(function(module) {
+
+  var searchController = {};
+
+  searchController.reveal = function() {
+    $('.tab-content').hide();
+    $('#search_section').fadeIn();
+  };
+
+  var mixtapeController = {};
+
+  mixtapeController.reveal = function() {
+    $('.tab-content').hide();
+    $('#mixtape').fadeIn();
+  };
+
+  var aboutController = {};
+
+  aboutController.reveal = function() {
+    $('.tab-content').hide();
+    $('#about').fadeIn();
+  };
+
+  module.searchController = searchController;
+  module.mixtapeController = mixtapeController;
+  module.aboutController = aboutController;
+})(window);
---
+++ scripts/controller/route.js
@@ -0,0 +1,6 @@
+
+page('/search_section', searchController.reveal);
+page('/mixtape', mixtapeController.reveal);
+page('/about', aboutController.reveal);
+
+page();
