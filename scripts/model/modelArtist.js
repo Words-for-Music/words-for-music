6db85eb405ed3454821d968cb59e70cd9fd0e57c
TomTimmel <tom.t.timmel@gmail.com>
2016-10-12 13:09:08 -0700

    api itunes working

--- scripts/model/modelArtist.js
+++ scripts/model/modelArtist.js
@@ -4,30 +4,35 @@
 
   musicData.allItunes = [];
 
-  getArtistData = function(){
+  musicData.getArtistData = function(){
     for(var i = 0; i < lyrics.allSongs.length; i++){
       var endpoint = encodeURI(lyrics.allSongs[i].primary_artist.name + '+' + lyrics.allSongs[i].title + '&limit=1');
-      console.log(endpoint);
+      // console.log(endpoint);
       $.ajax({
         url: 'itunes/' + endpoint
       }).done(function(data){
         var artistData = JSON.parse(data.text);
         console.log(artistData);
         musicData.allItunes.push(artistData);
-        // console.log(lyrics.allSongs[i].primary_artist.name);
+        musicData.useItunesData();
       }).fail(function(jqxhr, status){
         console.log('itunes AJAX request Call Failed: ', status, jqxhr);
       });
     };
   };
 
-  musicData.allItunes.forEach(function(element){
-    for (var i = 0; i < lyrics.allSongs.length; i++) {
-      if (lyrics.allSongs[i].title === element.trackName) {
-        lyrics.allSongs[i].artistViewUrl = element.artistViewUrl;
+  musicData.useItunesData = function(){
+    musicData.allItunes.forEach(function(element){
+      if (element.results.length){
+        for (var i = 0; i < lyrics.allSongs.length; i++) {
+          if (lyrics.allSongs[i].title === element.results[0].trackName && lyrics.allSongs[i].primary_artist.name === element.results[0].artistName) {
+            lyrics.allSongs[i].artistViewUrl = element.results[0].artistViewUrl;
+            lyrics.allSongs[i].previewUrl = element.results[0].previewUrl;
+          }
+        }
       }
-    }
-  });
+    });
+  };
 
   module.musicData = musicData;
 })(window);
